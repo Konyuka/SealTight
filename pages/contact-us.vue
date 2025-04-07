1027d20ab8058d9d18202f7729a011680859d04b
Konyuka <michaelsaiba84@gmail.com>
2025-04-07 15:18:24 +0300

    updates

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -18,12 +18,12 @@
                     </ul>
                 </div>
 
-                <!-- <div class="vectors">
+                <div class="vectors">
                     <img src="/assets/img/breadcrumb-vector-1.svg" alt="vector"
                         class="absolute -z-[1] pointer-events-none bottom-[34px] left-0 xl:left-auto xl:right-[90%]">
                     <img src="/assets/img/breadcrumb-vector-2.svg" alt="vector"
                         class="absolute -z-[1] pointer-events-none bottom-0 right-0 xl:right-auto xl:left-[60%]">
-                </div> -->
+                </div>
             </section>
             <!-- BREADCRUMB SECTION END -->
 
@@ -78,7 +78,7 @@
                                     <div class="txt">
                                         <span class="font-normal">Our Location</span>
                                         <h4 class="font-medium text-[24px] xxs:text-[22px]">
-                                            Delpack Business Complex <br>  Donholm | Nairobi | Kenya
+                                            Delpack Business Complex <br> Donholm | Nairobi | Kenya
                                         </h4>
                                     </div>
                                 </div>
@@ -137,6 +137,15 @@
                 </div>
             </section>
             <!-- CONTACT SECTION END -->
+
+            <!-- Google Map -->
+            <div class="relative w-full h-96">
+                <iframe class="absolute top-0 left-0 w-full h-full"
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12080.73732861526!2d-74.0059418!3d40.7127847!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM40zMDA2JzEwLjAiTiA3NMKwMjUnMzcuNyJX!5e0!3m2!1sen!2sus!4v1648482801994!5m2!1sen!2sus"
+                    frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+                </iframe>
+            </div>
+            <!-- Google Map -->
         </main>
     </div>
 </template>
